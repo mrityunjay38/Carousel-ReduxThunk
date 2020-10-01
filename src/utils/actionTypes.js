faa207f6f9ef7e979f354b518f92975f81549d33
Mrityunjay <mrityun.jay@indegene.com>
2020-10-01 17:09:21 +0530

    Dir structure setup

---
+++ src/utils/actionTypes.js
@@ -0,0 +1,10 @@
+const actionTypes = {
+    "ADD_SLIDES": "ADD_SLIDES",
+    "REMOVE_SLIDE": "REMOVE_SLIDE",
+    "CURRENT_SLIDE": "CURRENT_SLIDE",
+    "IS_PLAYING": "IS_PLAYING",
+    "NEXT_PREV": "NEXT_PREV",
+    "UPDATE_SLIDE": "UPDATE_SLIDE"
+}
+
+export default actionTypes;
